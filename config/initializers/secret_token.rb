dbf2b41e9a4bd8a063055c21aab3ea8976a0fc9a
Ramom Octavio Badillo Rivas <robr8@hotmail.com>
2012-10-02 17:37:01 -0500

    TA madre otra vez

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eltopi::Application.config.secret_token = 'be7b64c1ce02cd7cd29b6e2ec32e514a075a4035a91f41eb53878311977f639086955de269d5e1e2cf83390ba50be241f27a174d55caacd7f86232cc3e4478ff'
